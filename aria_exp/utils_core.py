145d662aae2639e3247dd9ac5d255a18f60fd630
Aria F <51890782+ariafyy@users.noreply.github.com>
2024-03-28 18:50:33 +0800

    #feat: cosine similarity

---
+++ aria_exp/utils_core.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+"""
+__info: "cosine similarity between two embeddings"
+"""
+
+import torch
+from torch import Tensor
+
+def utils_cos_sim(a: Tensor, b: Tensor):
+    """
+    computes similarity between two embeddings
+    Computes the cosine similarity cos_sim(a[i], b[j]) for all i and j.
+    :return: Matrix with res[i][j]  = cos_sim(a[i], b[j])
+    """
+    if not isinstance(a, torch.Tensor):
+        a = torch.tensor(a)
+
+    if not isinstance(b, torch.Tensor):
+        b = torch.tensor(b)
+
+    if len(a.shape) == 1:
+        a = a.unsqueeze(0)
+
+    if len(b.shape) == 1:
+        b = b.unsqueeze(0)
+
+    a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
+    b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
+    score = torch.mm(a_norm, b_norm.transpose(0, 1)).item()
+    return score
+
+
+
